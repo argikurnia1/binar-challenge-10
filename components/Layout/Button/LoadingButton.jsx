22fd048884175bc0d212dcf71fbb11876d921eb4
fauzan.gahara <fauzanfg20@gmail.com>
2022-11-30 00:11:29 +0700

    benerin button mas rifqi

--- components/Layout/Button/LoadingButton.jsx
+++ components/Layout/Button/LoadingButton.jsx
@@ -2,18 +2,18 @@ import { Button } from "react-bootstrap";
 import { useSelector } from "react-redux"
 
 
-const LoadingButton = ({title, onClick}) => {
+const LoadingButton = ({title, onClick, variant}) => {
   const loadingData = useSelector((state) => {
     return state.loadingReducer
   })
 
   if(loadingData.loadingStatus){
     return(
-        <Button variant="danger" disabled >Loading</Button>
+        <Button variant={variant} disabled >Loading</Button>
     )
   }else{
     return(
-        <Button variant="success" onClick={onClick}>{title}</Button>
+        <Button variant={variant} onClick={onClick}>{title}</Button>
       )
     }
   }
