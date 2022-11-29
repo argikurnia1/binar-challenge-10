3b868936a5694199c17c53183fc00bc22db619ae
argikurnia <argikurnia2@gmail.com>
2022-11-29 21:49:48 +0700

    feat: migrasi components profile :sparkles:

---
+++ components/Profile/ProfileGameHistory.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Card, Table } from "react-bootstrap";
+import { retrieveAllGames } from "../../actions/games";
+import stylesProfileGameHistory from "./ProfileGameHistory.module.css";
+
+const ProfileGameHistory = ({ userGameHistory, playerId }) => {
+  const [gameUserById, setGameUserById] = useState({});
+  useEffect(() => {
+    async function initData() {
+      let obj = {};
+      const allGames = await retrieveAllGames();
+      for (let val of allGames) {
+        obj[val.id] = val.data.game_title;
+      }
+
+      setGameUserById(obj);
+    }
+    initData();
+  }, []);
+
+  return (
+    <section className={stylesProfileGameHistory.sectionProfileGameHistory}>
+      <Card
+        style={{
+          backgroundColor: "#3B3838",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+        }}
+      >
+        {/* ProfileGameHistory Top */}
+        <div style={{ backgroundColor: "#464343" }}>
+          <Card.Header
+            className={stylesProfileGameHistory.profileGameHistoryHeader}
+          >
+            GAME HISTORY
+          </Card.Header>
+        </div>
+
+        {/* ProfileGameHistory Bottom */}
+        <Card.Body>
+          <Table bordered variant="dark">
+            <thead className="text-white">
+              <tr>
+                <th>No</th>
+                <th>Game</th>
+                <th>Game Time</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody className="text-white">
+              {userGameHistory?.map((user, i) => {
+                return (
+                  <tr key={i + 1}>
+                    <td>{i + 1}</td>
+                    <td>{gameUserById[user?.data?.game_id]}</td>
+                    <td>{user?.data?.time}</td>
+                    <td>{user?.data?.score}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </section>
+  );
+};
+
+export default ProfileGameHistory;
