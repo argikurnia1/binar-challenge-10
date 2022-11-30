c41a5d3f8dd15d586fff6979201bbfecf032b94f
fauzan.gahara <fauzanfg20@gmail.com>
2022-11-30 23:30:11 +0700

    whos your meme current game info done

--- pages/game/whosyourmeme/index.js
+++ pages/game/whosyourmeme/index.js
@@ -12,6 +12,7 @@ import { loadingAction } from "../../../redux/reducers/loadingReducer";
 import { insertGameScore } from "../../../actions/games"
 import { async } from "@firebase/util"
 import { playerRank, totalGameByUser, totalPointByUser } from "../../../actions/fb_database"
+import WYMGameInfo from "../../../components/Games/whosyourmeme/WYMGameInfo"
 
 const Dummy = () => {
   const dispatch = useDispatch()
@@ -98,24 +99,41 @@ const Dummy = () => {
     }
   ])
   const [random, setRandom] = useState(0)
-
+  const [gameInfo, setGameInfo] = useState({
+    ronde: 0,
+    status: "let the game decide",
+    score: 0
+  })
 
   const handleGame= async () =>{
     dispatch(loadingAction.toggleLoadingStatus())
     let randomize = Math.floor((Math.random() * 12) + 1)
     setRandom(randomize)
     insertGameScore("-NI6wC-QCtYu4TMTzgt0",userLoginData[0]?.id,memeBase[randomize].score)
-    dispatch(loadingAction.toggleLoadingStatus())
     playerRank(userLoginData[0]?.id)
     totalPointByUser(userLoginData[0]?.id)
     totalGameByUser(userLoginData[0]?.id)
+    handleInfo(randomize)
+    dispatch(loadingAction.toggleLoadingStatus())
   }
-
-  useState(() =>{
-    
-  },[])
+  const handleInfo = (randomize) =>{
+    let tempRonde = gameInfo.ronde + 1
+    let tempStatus = ""
+    let tempScore = gameInfo.score + memeBase[randomize].score
+    if(tempScore < 0){
+      tempStatus = "you are bad luck"
+    }else{
+      tempStatus = "you are good luck"
+    }
+    setGameInfo({
+      ronde: tempRonde,
+      status: tempStatus,
+      score : tempScore
+    })
+  }
   return (
     <div>
+      
       <Navbar bgColor="#4A4A5C" />
       <Container style={{ color: "coral"}}>
         <div className={style.bodyButton}>
@@ -135,6 +153,9 @@ const Dummy = () => {
             title="PLAY"
             onClick={()=>handleGame()}
             varriant="success"
+            />
+            <WYMGameInfo
+            props={gameInfo}
             />
           </div>
         </div>
